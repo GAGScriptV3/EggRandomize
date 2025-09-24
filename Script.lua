8b30bc8d67ec12549154b5cea220fd3c8ef846ad
GAGScriptV3 <osaragihub@gmail.com>
2025-09-24 23:34:37 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,2 @@
-https://raw.githubusercontent.com/GAGscriptbyyash/New-Obs/refs/heads/main/darkspawner
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGscriptbyyash/New-Obs/refs/heads/main/darkspawner"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGScriptV3/egg/refs/heads/main/lua"))()
