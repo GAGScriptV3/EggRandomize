bf3d58a62e5a538dca88b96f074ec12ba095be05
GAGScriptV3 <osaragihub@gmail.com>
2025-09-27 09:16:48 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGscriptbyyash/New-Obs/refs/heads/main/darkspawner"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGScriptV3/egg/refs/heads/main/lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGScriptV3/lua/refs/heads/main/antileave"))()
